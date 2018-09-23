e7ef693e04b77c1decbcab1c470423ae37afcfdf
Peter Bui <pbui@bx612.space>
2018-09-22 22:56:38 -0400

    Symlink vsv to service and vpm to package

--- scripts/setup-usr-local-scripts.sh
+++ scripts/setup-usr-local-scripts.sh
@@ -13,7 +13,13 @@ do_root_check || exit 1
 
 rm -f /usr/local/bin/package /usr/local/bin/service # Delete old scripts
 
-for script in vpm vsv; do
-    printf "Installing $script ... "
-    run_check install_file usr/local/bin/$script -oroot -groot -m755 || exit 1
+for script in vpm=package vsv=service; do
+    src=${script%=*}
+    lnk=${script#*=}
+
+    printf "Installing $src... "
+    run_check install_file usr/local/bin/$src -oroot -groot -m755 || exit 1
+
+    printf "Symlinking $lnk... "
+    run_check ln -sf $src /usr/local/bin/$lnk
 done
