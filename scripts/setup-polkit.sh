db62ac771c9e6df8612886cf2fb3583b22ebab5f
Peter Bui <pbui@bx612.space>
2019-08-14 10:30:32 -0400

    polkit: cleanup old rules

--- scripts/setup-polkit.sh
+++ scripts/setup-polkit.sh
@@ -16,3 +16,13 @@ for rules in files/etc/polkit-1/rules.d/*.rules; do
     printf "Installing $rules... "
     run_check install_file etc/polkit-1/rules.d/$(basename $rules) -oroot -groot -m644|| exit 1
 done
+
+# Cleanup
+
+for rules in 90-consolekit; do
+    path=/etc/polkit-1/rules.d/$rules.rules
+    if [ -r $path ]; then 
+	printf "Removing $rules... "
+	run_check rm -f $path || exit 1
+    fi
+done
