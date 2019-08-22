652d10e7bc3e624b83298e35ae5e25e5fd608c40
Peter Bui <pbui@bx612.space>
2019-08-22 10:30:28 -0400

    Add udev script

---
+++ scripts/setup-udev.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# setup-udev.sh: Setup udev rules
+
+. $(dirname $0)/common.sh
+
+# Root Check
+
+do_root_check || exit 1
+
+# Install rules
+
+for rules in files/etc/udev/rules.d/*.rules; do
+    printf "Installing $rules... "
+    run_check install_file etc/udev/rules.d/$(basename $rules) -oroot -groot -m644|| exit 1
+done
+
+# Reload rules
+
+printf "Reload rules... "
+run_check udevadm control --reload
+
+printf "Trigger rules... "
+run_check udevadm trigger
