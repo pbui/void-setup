c1f4dff9a91d4e19ddec05a17903783be7684667
Peter Bui <pbui@bx612.space>
2020-05-03 17:25:05 -0400

    Add popcorn

---
+++ scripts/setup-popcorn.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# setup-popcorn.sh: Setup popcorn
+#
+
+. $(dirname $0)/common.sh
+
+# Root Check
+
+do_root_check || exit 1
+
+# Install PopCorn package
+
+if ! command -v popcorn > /dev/null 2>&1; then
+    printf "Installing cronie package ... "
+    run_check xbps-install -Sy PopCorn || exit 1
+fi
+
+# Install PopCorn cronjob
+
+printf "Install popcorn cron.daily... "
+run_check install_file etc/cron.daily/popcorn -oroot -groot -m755 || exit 1
